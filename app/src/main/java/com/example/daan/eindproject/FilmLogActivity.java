0741a9f7ce9fa606e97933160eb6491748207901
danert <daanvisser98@hotmail.com>
2019-01-09 13:54:36 +0100

    film log

---
+++ app/src/main/java/com/example/daan/eindproject/FilmLogActivity.java
@@ -0,0 +1,13 @@
+package com.example.daan.eindproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FilmLogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_film_log);
+    }
+}

fd3e0ca1196866c74978ce5fc2d0ca9620690843
danert <daanvisser98@hotmail.com>
2019-01-09 14:27:01 +0100

    woensdag 9 januari

--- app/src/main/java/com/example/daan/eindproject/MovieInfoActivity.java
+++ app/src/main/java/com/example/daan/eindproject/MovieInfoActivity.java
@@ -1,5 +1,6 @@
 package com.example.daan.eindproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,17 @@ public class MovieInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_info);
+    }
+
+    // move to filmlogactivity
+    public void logFilm() {
+
+        Intent intent = new Intent(MovieInfoActivity.this, FilmLogActivity.class);
+
+        // give movie title and poster to intent
+        intent.putExtra("title", title);
+        intent.putExtra("poster", poster);
+
+        startActivity(intent);
     }
 }

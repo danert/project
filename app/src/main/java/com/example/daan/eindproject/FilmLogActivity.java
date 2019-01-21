eeb4316cdc742efce001568cfb3926a7c8e26ae9
danert <daanvisser98@hotmail.com>
2019-01-21 14:05:56 +0100

    font design

--- app/src/main/java/com/example/daan/eindproject/FilmLogActivity.java
+++ app/src/main/java/com/example/daan/eindproject/FilmLogActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Movie;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -15,8 +16,14 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.concurrent.TimeUnit;
 
@@ -25,6 +32,7 @@ public class FilmLogActivity extends AppCompatActivity {
     MovieInfo movieInfo;
     float starRating;
     String reviewText;
+    int removalId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -90,8 +98,89 @@ public class FilmLogActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+        // if movie in watchlist, remove it from watchlist
+        if (checkWatchlist() != 0) {
+
+            Log.i("AJDHAHDKBAWKUDGWAUGDAWUDWAGYY", "AIUDHAWUIWUIWADH");
+
+            // request to delete movie entry (https://www.itsalif.info/content/android-volley-tutorial-http-get-post-put)
+            RequestQueue deleteQueue = Volley.newRequestQueue(getApplicationContext());
+
+            String deleteUrl = String.format("https://ide50-danert.legacy.cs50.io:8080/daanwatchlist/%d", removalId);
+
+            StringRequest dr = new StringRequest(Request.Method.DELETE, deleteUrl,
+                    new Response.Listener<String>()
+                    {
+                        @Override
+                        public void onResponse(String response) {
+                        }
+                    },
+                    new Response.ErrorListener()
+                    {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+            );
+            deleteQueue.add(dr);
+        }
+
         // move back to homepage
         Intent intent = new Intent(FilmLogActivity.this, HomepageActivity.class);
         startActivity(intent);
+    }
+
+    // check if movie is in watchlist, if it is it returns the corresponding id, if it isn't it returns 0
+    public int checkWatchlist() {
+
+        removalId = 0;
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+
+        // DAAN VERVANGEN DOOR GEBRUIKERSNAAM LATER!!!
+        String url = "https://ide50-danert.legacy.cs50.io:8080/daanwatchlist";
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+
+            // when watchlist was received
+            @Override
+            public void onResponse(JSONArray response) {
+
+                String movieId = movieInfo.getMovieId();
+
+                // check every entry
+                for (int i = 0; i < response.length(); i++) {
+
+                    try {
+
+                        // grab entry
+                        JSONObject movieEntry = response.getJSONObject(i);
+
+                        // grab movie id that needs to be checked
+                        String checkId = movieEntry.getString("movieId");
+
+                        // if right movie id, stop searching
+                        if (movieId.equals(checkId)) {
+
+                            removalId = movieEntry.getInt("id");
+                            break;
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+        queue.add(jsonArrayRequest);
+
+        Log.i("removalId", Integer.toString(removalId));
+        return removalId;
     }
 }
